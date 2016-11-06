91f30aab392fcd9abf26671e670eb5d06711d030
MashaLah <lahoska91@mail.ru>
2016-11-06 20:29:57 +0300

    Add Customer.

---
+++ Task1/Customer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Customer:IFormattable
+    {
+        public string Name { get; set; }
+        public string ContactPhone { get; set; }
+        public decimal Revenue { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Customer(string name, string contactPhone, decimal revenue)
+        {
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = revenue;
+        }
+
+        /// <summary>
+        /// String representation of Customer object.
+        /// </summary>
+        public override string ToString() => 
+            ToString("G", CultureInfo.InvariantCulture);
+
+        /// <summary>
+        /// String representation of Customer object using the specified format.
+        /// </summary>
+        public string ToString(string format) =>
+            ToString(format, CultureInfo.InvariantCulture);
+
+        /// <summary>
+        /// String representation of Customer object using the specified format and culture-specific format information.
+        /// </summary>
+        public string ToString(string format, IFormatProvider provider)
+        {
+            if (String.IsNullOrEmpty(format)) format = "G";
+            if (provider == null) provider = CultureInfo.InvariantCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G": return $"Customer record: {Name}, {Revenue.ToString("0,0.00", provider)}, {ContactPhone}";
+                case "A": return $"Customer record: {ContactPhone}";
+                case "B": return $"Customer record: {Name}, {Revenue.ToString("0,0.00", provider)}";
+                case "C": return $"Customer record: {Name}";
+                case "D": return $"Customer record: {Revenue}";
+                default:
+                    throw new FormatException(String.Format("The {0} format string is not supported.", format));
+
+            }
+        }
+    }
+}
---
+++ Task1Tests/CustomerTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+using NUnit.Framework;
+
+namespace Task1Tests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+
+        /// <summary>
+        /// A test for ToString with valid data.
+        /// </summary>
+        [TestCase("G", "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("A", "Customer record: +1 (425) 555-0100")]
+        [TestCase("B", "Customer record: Jeffrey Richter, 1,000,000.00")]
+        [TestCase("C", "Customer record: Jeffrey Richter")]
+        [TestCase("D", "Customer record: 1000000")]
+        public void ToString_ValidData_ValidResult(string format, string expected)
+        {
+            string actual = customer.ToString(format);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// A test for ToString without parameter.
+        /// </summary>
+        [TestCase("Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        public void ToString_NoParameter_ValidData(string expected)
+        {
+            string actual = customer.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// A test for ToString with invalid format.
+        /// </summary>
+        [Test]
+        public void ToString_InvalidFormat_ThrowsFormatException()
+        {
+            Assert.Throws<FormatException>(() => customer.ToString("Q"));
+        }
+    }
+}
